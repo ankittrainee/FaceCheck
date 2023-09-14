778f74f54a5eb860366f8bbbf77c784f67e38b08
Rituraj Gurjar <rituraj.gurjar@lemosys.com>
2023-09-14 15:52:32 +0530

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,79 @@
-import { useRef, useEffect } from 'react';
-import './App.css';
-import * as faceapi from 'face-api.js';
-import { Container } from 'react-bootstrap';
+import {useRef,useEffect} from 'react'
+import './App.css'
+import * as faceapi from 'face-api.js'
+import { Container } from 'react-bootstrap'
 
-function App() {
-  const videoRef = useRef();
-  const canvasRef = useRef();
-  const aspectRatio = (940 + 650) / 2;
-
+function App(){
+  const videoRef = useRef()
+  const canvasRef = useRef()
+  const aspectRatio = (940 + 650) / 2; 
   // LOAD FROM USEEFFECT
-  useEffect(() => {
-    startVideo();
-    videoRef && loadModels();
-  }, []);
+  useEffect(()=>{
+    startVideo()
+    videoRef && loadModels()
+
+  },[])
+
+
 
   // OPEN YOU FACE WEBCAM
-  const startVideo = () => {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then((currentStream) => {
-        videoRef.current.srcObject = currentStream;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const startVideo = ()=>{
+    navigator.mediaDevices.getUserMedia({video:true})
+    .then((currentStream)=>{
+      videoRef.current.srcObject = currentStream
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
   }
-
   // LOAD MODELS FROM FACE API
-  const loadModels = () => {
+
+  const loadModels = ()=>{
     Promise.all([
       // THIS FOR FACE DETECT AND LOAD FROM YOU PUBLIC/MODELS DIRECTORY
       faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
       faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
       faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
       faceapi.nets.faceExpressionNet.loadFromUri("/models")
-    ])
-    .then(() => {
-      faceMyDetect();
-    });
+
+      ]).then(()=>{
+      faceMyDetect()
+    })
   }
 
-  const faceMyDetect = () => {
-    setInterval(async () => {
+  const faceMyDetect = ()=>{
+    setInterval(async()=>{
       const detections = await faceapi.detectAllFaces(videoRef.current,
-        new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
 
-      // Clear the canvas before each update
-      const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
-      context.clearRect(0, 0, canvas.width, canvas.height);
+      // DRAW YOU FACE IN WEBCAM
+      canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(videoRef.current)
+      faceapi.matchDimensions(canvasRef.current,{
+        width:940,
+        height:650
+      })
 
-      // If faces are detected, draw squares around each detected face
-      if (detections && detections.length > 0) {
-        detections.forEach((detection) => {
-          const { x, y, width, height } = detection.detection.box;
-          const size = Math.max(width, height); // Determine the size of the square
-          context.strokeStyle = 'red'; // Set the square color
-          context.lineWidth = 2;
-          context.strokeRect(x, y, size, size); // Draw the square
-        });
-      }
-    }, 1000);
+      const resized = faceapi.resizeResults(detections,{
+         width:10,
+        height:650
+      })
+
+      faceapi.draw.drawDetections(canvasRef.current,resized)
+      faceapi.draw.drawFaceLandmarks(canvasRef.current,resized)
+      faceapi.draw.drawFaceExpressions(canvasRef.current,resized)
+
+
+    },1000)
   }
 
   return (
     <div className="myapp">
-      <h1>Face Detection</h1>
-      <video className="responsive-video" crossOrigin="anonymous" ref={videoRef} autoPlay></video>
-      <canvas
-        ref={canvasRef}
-        width={window.innerWidth}
-        height={window.innerHeight}
-        className="appcanvas"
-      />
-    </div>
-  );
+    <h1>Face Detection</h1>
+    <video className="responsive-video" crossOrigin="anonymous" ref={videoRef} autoPlay></video>
+    <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} className="appcanvas" />
+  </div>
+    )
+
 }
 
 export default App;
